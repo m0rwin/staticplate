329472b8f74225bf705724aae81027fb9231fca7
Dipun Mistry <dipunm@gmail.com>
2017-12-02 14:10:13 +0000

    fixed css bug and added error messages

--- scss.js
+++ scss.js
@@ -15,9 +15,15 @@ function(error, result) { // node-style callback from v3.0.0 onwards
     if(!error){
         // No errors during the compilation, write this result on the disk
         fs.writeFile('./css/style.css', result.css, function(err){
-        if(!err){
-            //file written on disk
-        }
+            if(!err){
+                //file written on disk
+            } else {
+                console.log('unable to write to file: ' + './css/style.css');
+                console.log(err);
+            }
         });
+    } else {
+        console.log('unable to interpret scss file: ' + error.file)
+        console.log('' + error.formatted);
     }
 });
